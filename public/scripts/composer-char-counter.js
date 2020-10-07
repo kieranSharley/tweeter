6229f2b03fcdb4030fe548fc07971368cb646d00
kieranSharley <ksharley9@gmail.com>
2020-10-07 01:38:02 +0000

    composer-char-counter.js added

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,33 @@
+
+
+$(document).ready(function () {
+  // --- our code goes here ---
+
+  $("#tweet-text").keyup(function () {
+    let maxChars = 140;
+    let charsLength = $(this).val().length;
+    let charLeftover = maxChars - charsLength;
+    let theDiv = $(this).siblings()[1];
+    let output = $(theDiv).children(".counter");
+    //console.log($(this).siblings()[1].find(".counter"))
+    $(output).html(charLeftover);
+    // if(charsLength < 140) {
+
+    //   else{}
+    }
+
+
+
+  });
+
+
+  $("#btn").on('click', function () {
+    //The this keyword is a reference to the button
+  });
+  // using jQuery and an appropriate selector, 
+  // register an event handler to the textarea element 
+  // for the form inside of the .new-tweet section.
+
+
+  //borderbackgroundand margin set in new-tweet.css
+});
--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ app.use(express.static("public"));
 // The in-memory database of tweets. It's a basic object with an array in it.
 const db = require("./lib/in-memory-db");
 
+
 // The `data-helpers` module provides an interface to the database of tweets.
 // This simple interface layer has a big benefit: we could switch out the
 // actual database it uses and see little to no changes elsewhere in the code
@@ -21,7 +22,6 @@ const db = require("./lib/in-memory-db");
 // Because it exports a function that expects the `db` as a parameter, we can
 // require it and pass the `db` parameter immediately:
 const DataHelpers = require("./lib/data-helpers.js")(db);
-
 // The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
 // so it can define routes that use it to interact with the data layer.
 const tweetsRoutes = require("./routes/tweets")(DataHelpers);
@@ -32,3 +32,4 @@ app.use("/tweets", tweetsRoutes);
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
+
