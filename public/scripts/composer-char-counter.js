4b1929530350c2a57dced947d025b1684eb8cd02
kieranSharley <ksharley9@gmail.com>
2020-10-10 01:32:27 +0000

    load screenshots

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,48 +1,22 @@
-
-
 $(document).ready(function () {
-  // --- our code goes here ---
 
+  //Helper function - Changes the character counter displayed at the bottom of the text input.
   $("#tweet-text").keyup(function () {
     let maxChars = 140;
     let charsLength = $(this).val().length;
     let charLeftover = maxChars - charsLength;
-    const counter = $(this).siblings("div.footer").children('output')
-    //const turnRed =  counter.css(color, red)
-   $(counter).html(charLeftover);
+    const counter = $(this).siblings("div.footer").children('output');
+    $(counter).html(charLeftover);
+    if (charsLength > 140 || charsLength === 0) {
 
-   if(charsLength > 140 || charsLength === "") {
-     
-     $(counter.css('color', 'red'));
-     $(counter).html(charLeftover);
-   }
-
-    //   counter.css(color, red)
-    //   $(counter).html(charLeftover);
-
-    // } else {
-    // let theDiv = $(this).siblings("div.footer");
-    // let output = $(theDiv).children(".counter");
-    //console.log($(this).siblings()[1].find(".counter"))
-    
-    //}
-
-    //   else{}
-    //}
-    
-    // .css('color', 'red')
-
-
+      $(counter.css('color', 'red'));
+      $(counter).html(charLeftover);
+    }
+    else {   
+      $(counter.css('color', 'black'));
+      $(counter).html(charLeftover);} 
   });
+});
 
-
-  $("#btn").on('click', function () {
-    //The this keyword is a reference to the button
-  });
-  // using jQuery and an appropriate selector, 
-  // register an event handler to the textarea element 
-  // for the form inside of the .new-tweet section.
-
-
-  //borderbackgroundand margin set in new-tweet.css
+$("#btn").on('click', function () {
 });
